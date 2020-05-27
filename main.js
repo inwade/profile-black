eca4867808437e5e1eb5a7666d3d031c4f0134e2
inwein <39739420+inwein@users.noreply.github.com>
2020-05-27 16:17:26 +0300

    block skills done

--- main.js
+++ main.js
@@ -29,3 +29,33 @@ phoneIcon.onmouseout = mouseOut;
 mailIcon.onmouseover = mouseOver;
 mailIcon.onmouseout = mouseOut;
 
+//add fade-up to About me block on scroll
+let myInfoBlock = document.querySelector('.my-info-block')
+let contactBlock = document.querySelector('.contacts-block')
+let myInfoCoord = myInfoBlock.getBoundingClientRect();
+let contactCoord = contactBlock.getBoundingClientRect();
+let scrollAboutMeToAppear = (myInfoCoord.height + contactCoord.height) / 2;
+let stoper = false
+let filler = false
+let aboutMeBlock = document.querySelector('.about-me-block')
+
+
+// add line filings on the skills block
+let aboutMeBlockCoord = aboutMeBlock.getBoundingClientRect()
+let scrollForFill = (myInfoCoord.height + contactCoord.height + aboutMeBlockCoord.height) / 2;
+let skillLineFill = document.querySelectorAll('.fill-skill-line')
+
+
+document.onscroll = function () {
+    if (stoper == false && window.pageYOffset >= scrollAboutMeToAppear) {
+    stoper = true
+    aboutMeBlock.setAttribute('data-aos', 'fade-up')
+};
+    if (filler == false && window.pageYOffset >= scrollForFill) {
+        filler = true
+        skillLineFill.forEach((skillLine) => {
+            let newWidth = skillLine.dataset.width
+            skillLine.style.width = newWidth + '%'
+        })     
+    }
+}
